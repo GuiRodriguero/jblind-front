8608477ff2cf80c5755a3b4084d9234df1c31258
GuiRodriguero <gui.rodriguero@hotmail.com>
2026-05-27 00:49:07 -0300

    Create CoffeAnimatedIcon

---
+++ src/features/timer/components/icon/CoffeeAnimatedIcon.tsx
@@ -0,0 +1,10 @@
+import './animated-coffee.css';
+import { Coffee } from 'lucide-react';
+
+export function CoffeeAnimated() {
+  return (
+    <div className="relative">
+      <Coffee className="w-10 h-10 coffee-icon" />
+    </div>
+  );
+}
